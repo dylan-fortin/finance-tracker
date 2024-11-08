8ee337c825ad1d15e1aab860acdd800430672e4a
dylan-fortin <167590044+dylan-fortin@users.noreply.github.com>
2024-11-08 10:40:54 -0500

    add widget in dashboard

--- src/app/component/page/dashboard/dashboard.component.ts
+++ src/app/component/page/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import Chart  from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,8 +10,40 @@ import { AuthService } from '@auth0/auth0-angular';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  public chart: any;
 
   constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.createDonutChart();
+  }
+
+  createDonutChart(): void {
+    this.chart = new Chart("MyChart", {
+      type: 'doughnut', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['Red', 'Blue','Green'],
+           datasets: [{
+    label: 'My First Dataset',
+    data: [300, 240, 100],
+    backgroundColor: [
+      'red',
+      'blue',
+      'green'        
+    ],
+    
+    hoverOffset: 4
+  }],
+      },
+      options: {
+        aspectRatio:2.5,
+        cutout: "70%",
+      },
+
+    });
+  }
 
 }
